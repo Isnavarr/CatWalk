ead1f38ec910bd29e756b22bead1bbe971f20281
Jessica Lam <jess.lam948@gmail.com>
2021-02-28 15:15:04 -0800

    update wrapper code

--- Wrapper_code/build.sh
+++ Wrapper_code/build.sh
@@ -3,5 +3,5 @@
 # TODO Add linker dependencies and other compile flags
 
 #g++ PinTracerWrapper.cpp -o wrapper -fno-split-stack
-g++ -L /home/jessicalam/Desktop/CatWalk/jess_toy/ -Wall MyWrapper.cpp -o wrapper -l shared
+g++ -L /home/jessicalam/Desktop/CatWalk/Toy2/ -Wall MyWrapper.cpp -o wrapper -lshared
 
