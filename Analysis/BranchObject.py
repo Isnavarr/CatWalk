8ee3c646d7bfb26653aac8c62cc5a4e53a59578b
BritneyVi <Britneythaovi@gmail.com>
2021-04-18 16:12:28 -0700

    Update BranchObject.py
    Added averages for how often a branch would be visited in a trace

--- Analysis/BranchObject.py
+++ Analysis/BranchObject.py
@@ -3,29 +3,40 @@ class BranchObject:
         self.address = address # addr offset 4
         self.addrType = addrType # addrtype 'm' or 'b' or 'r'
         self.insMap = {} # map {4: {1}, 6: {2, 3}}  set* key instruction, value set of testcases
-        self.totalTestcases = [] # total testcases
-        self.visitTotal = 0; # how many times this branch is visited in one trace
+        self.totalTestcases = 0 # total testcases
+        self.visitTotal = {} # 
+        self.count = 0
         self.testcase = -1
 
     def update(self, inscount, testInput): # update(inscount, input):
         if(testcase != self.testcase)
             self.totalTestcases += 1 # totalTestcase += 1
             self.testcase = testInput
+            self.visitTotal[count] = {testcase}
+            self.count = 0
         if inscount not in self.insMap: # does map have this inscount?
             self.insMap[inscount] = {testInput} # map[inscount] append input map[iscount].add(#)
         else:
             self.insMap[inscount].add(testInput) # map[inscount] = {input}
 
     def updateVisitTotal(self):
-        self.visitTotal[testcase] += 1
-    
-    def updateUniqueVisit(self, ) # oh no how do we know which addresses we'll need to update?? 
+        self.count += 1
 
     def printProportions(self):
         print(self.addrType + ' addr: ' + str(self.address) + ' total: ' + str(self.totalTestcases))
         for inscount in self.insMap: #   for each inscount in map:
             # print(str(inscount) + ': ' + str(len(self.insMap[inscount])) + " " + str(self.totalTestcases))
             print(str(inscount) + ': ' + str(float(len(self.insMap[inscount])) / (self.totalTestcases)))
+        for count in self.visitTotal:
+            print(self.address + ' ran ' + (self.count) + 'times: ' 
+            + str(float(len(self.visitTotal[count])) / (self.totalTestcases)) + ' this percentage of the time')
+            
+            # add the total # of times the branch was visited overall
+            avg += (count * str(float(len(self.visitTotal[count]))))
+            total += (str(float(len(self.visitTotal[count]))))
+
+        avg = (avg / total)
+        print( (self.address) + ' runs an average of ' + avg + ' times when it is run.')
             
             
             
